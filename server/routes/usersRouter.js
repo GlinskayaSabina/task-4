e2a0f1e05d031d26ddb98022552941081062b0d9
GlinskayaSabina <sabinagli45@gmail.com>
2022-11-21 16:03:24 +0400

    feat: authorization logic

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,10 +1,11 @@
 const Router = require("express");
 const usersController = require("../controllers/usersController");
 const router = new Router();
+const authMiddleware = require("../middlwaere/authMiddlware");
 
 router.post("/registration", usersController.registration);
 router.post("/login", usersController.login);
-router.get("/auth", usersController.check);
+router.get("/auth", authMiddleware, usersController.check);
 
 router.delete("/");
 
